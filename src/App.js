c37094e52dc7f215fb9c910f9c520198a3a3e152
okoid721 <okoid721@gmail.com>
2024-04-05 21:32:06 +0100

    recipe

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
 import Nav from './components/Nav';
 import Home from './pages/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import Auth from './pages/Auth';
 
 function App() {
+  const location = useLocation();
+  const isAuthRoute = location.pathname.startsWith('/auth');
+
+  // Redirect to the login page if the user is not authenticated
+  const isAuthenticated = false; // Replace this with your authentication logic
+  if (!isAuthenticated && !isAuthRoute) {
+    return <Navigate to="/auth/login" replace />;
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/auth/*" element={<Auth />} />
-          <Route path="/*" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      {!isAuthRoute && <Nav />} {/* Render Nav only if not on auth route */}
+      <Routes>
+        <Route path="/auth/*" element={<Auth />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
     </div>
   );
 }
