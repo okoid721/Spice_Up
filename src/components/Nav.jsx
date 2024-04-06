2a4716fe559a3af61647ff3e6d6484def1a34271
okoid721 <okoid721@gmail.com>
2024-04-06 22:26:43 +0100

    recipe

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,6 +9,7 @@ import { MdDinnerDining } from 'react-icons/md';
 import { GiRoastChicken } from 'react-icons/gi';
 import { IoIosAdd } from 'react-icons/io';
 import { Link } from 'react-router-dom';
+import Navsmall from './Navsmall';
 
 const Nav = () => {
   const [open, setOpen] = React.useState(false);
@@ -33,94 +34,72 @@ const Nav = () => {
     };
   }, []);
   return (
-    <div className=" w-full bg-[aliceblue]  shadow-xl py-6 sticky top-0 z-10 lg:px-6 px-2 ">
-      <div className="flex flex-row items-center justify-between">
-        <Link to="/home">
-          <h1 className="font-extrabold hidden lg:flex text-3xl ">SPICE_☝🏿</h1>
-        </Link>
-        <div className=" items-center hidden lg:flex bg-white w-fit lg:px-7 px-2 py-2 lg:font-extrabold rounded-lg shadow-xl">
-          <input
-            type="search"
-            placeholder="Search...."
-            className="bg-transparent outline-none rounded-none"
-          />
-          <FaSearch size={30} className="" />
-        </div>
-        <div className="">
-          <Link to="/auth/login">
-            <button className="lg:text-2xl hidden font-bold hover:underline lg:flex flex-col lg:flex-row items-center py-2 px-6">
-              <IoPerson />
-              Logout
-            </button>
-          </Link>
-        </div>
-      </div>
-      <div className=" flex flex-row justify-between px-2 mb-2">
-        <div
-          className=" flex lg:items-center items-start flex-col justify-between lg:flex-row "
-          onClick={toggleMenu}
-        >
-          <button className=" w-8 h-8" onClick={() => setOpen(!open)}>
-            <FaBarsStaggered size={30} />
-          </button>
-          <div>
-            {open && (
-              <motion.div
-                className="lg:flex-row flex-col gap-x-36 mt-8 font-extrabold   lg:flex cursor-pointer"
-                initial={{ x: -1000 }}
-                animate={{ x: 0 }}
-                transition={{ duration: 1 }}
-              >
-                <Link to="/home/breakfast">
-                  <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
-                    <MdFreeBreakfast size={30} />
-                    Breakfast
-                  </h1>
-                </Link>
-                <Link to="/home/lunch">
-                  <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
-                    <MdLunchDining size={30} />
-                    Lunch
-                  </h1>
-                </Link>
-                <Link to="/home/dinner">
-                  <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
-                    <MdDinnerDining size={30} />
-                    Dinner
-                  </h1>
-                </Link>
-                <Link to="/home/smallchop">
-                  <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
-                    <GiRoastChicken size={30} />
-                    SmallChop
-                  </h1>
-                </Link>
-                <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
-                  <IoIosAdd size={30} />
-                  Add Your MEAL
-                </h1>
-                <Link to="/auth/login">
-                  <button className="lg:text-2xl lg:hidden  font-bold hover:underline flex lg:flex-col flex-row items-center text-[20px] ">
-                    <IoPerson />
-                    Logout
-                  </button>
-                </Link>
-              </motion.div>
-            )}
+    <div>
+      <div className=" hidden lg:block">
+        <div className=" w-full bg-[aliceblue]  shadow-xl py-6 sticky top-0 z-10 lg:px-6 px-2 ">
+          <div className="flex flex-row items-center justify-between">
+            <Link to="/home">
+              <h1 className="font-extrabold hidden lg:flex text-3xl ">
+                SPICE_☝🏿
+              </h1>
+            </Link>
+            <div className=" items-center hidden lg:flex bg-white w-fit lg:px-7 px-2 py-2 lg:font-extrabold rounded-lg shadow-xl">
+              <input
+                type="search"
+                placeholder="Search...."
+                className="bg-transparent outline-none rounded-none"
+              />
+              <FaSearch size={30} className="" />
+            </div>
+            <div className="">
+              <Link to="/auth/login">
+                <button className="lg:text-2xl hidden font-bold hover:underline lg:flex flex-col lg:flex-row items-center py-2 px-6">
+                  <IoPerson />
+                  Logout
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className=" lg:flex flex-row hidden mt-4 items-center justify-around w-full ">
+            <Link to="/home">
+              <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+                <MdFreeBreakfast size={30} />
+                Breakfast
+              </h1>
+            </Link>
+            <Link to="/home/lunch">
+              <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+                <MdLunchDining size={30} />
+                Lunch
+              </h1>
+            </Link>
+            <Link to="/home/dinner">
+              <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+                <MdDinnerDining size={30} />
+                Dinner
+              </h1>
+            </Link>
+            <Link to="/home/smallchop">
+              <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+                <GiRoastChicken size={30} />
+                SmallChop
+              </h1>
+            </Link>
+            <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+              <IoIosAdd size={30} />
+              Add Your MEAL
+            </h1>
+            <Link to="/auth/login">
+              <button className="lg:text-2xl lg:hidden  font-bold hover:underline flex lg:flex-col flex-row items-center text-[20px] ">
+                <IoPerson />
+                Logout
+              </button>
+            </Link>
           </div>
         </div>
-        <Link to="/home">
-          <h1 className="font-extrabold lg:hidden block text-3xl ">SPICE_UP</h1>
-        </Link>
       </div>
-      <div className=" items-center  flex lg:hidden bg-white w-fit lg:px-7 px-2 py-2 lg:font-extrabold rounded-lg shadow-xl">
-        <input
-          type="search"
-          placeholder="Search...."
-          className="bg-transparent outline-none rounded-none"
-        />
-        <FaSearch size={30} className="" />
-      </div>
+
+      <Navsmall />
     </div>
   );
 };
---
+++ src/components/Navsmall.jsx
@@ -0,0 +1,171 @@
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { IoPerson } from 'react-icons/io5';
+import { FaBarsStaggered } from 'react-icons/fa6';
+import { motion } from 'framer-motion';
+import { MdFreeBreakfast } from 'react-icons/md';
+import { MdLunchDining } from 'react-icons/md';
+import { MdDinnerDining } from 'react-icons/md';
+import { GiRoastChicken } from 'react-icons/gi';
+import { IoIosAdd } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+
+const Navsmall = () => {
+  const [open, setOpen] = React.useState(false);
+  const [login, setLogin] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const menuRef = React.useRef(null);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  React.useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+  return (
+    <div className=" w-full bg-[aliceblue] lg:hidden block  shadow-xl py-6 sticky top-0 z-10 lg:px-6 px-2 ">
+      <div className=" flex flex-row justify-between px-2 mb-2">
+        <div
+          className=" flex lg:items-center items-start flex-col justify-between lg:flex-row "
+          onClick={toggleMenu}
+        >
+          <button className=" w-8 h-8" onClick={() => setOpen(!open)}>
+            <FaBarsStaggered size={30} />
+          </button>
+          <div>
+            {open && (
+              <motion.div
+                className="lg:flex-row flex-col gap-x-36 mt-8 font-extrabold   lg:flex cursor-pointer"
+                initial={{ x: -1000 }}
+                animate={{ x: 0 }}
+                transition={{ duration: 1 }}
+              >
+                <Link to="/home/breakfast">
+                  <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+                    <MdFreeBreakfast size={30} />
+                    Breakfast
+                  </h1>
+                </Link>
+                <Link to="/home/lunch">
+                  <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+                    <MdLunchDining size={30} />
+                    Lunch
+                  </h1>
+                </Link>
+                <Link to="/home/dinner">
+                  <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+                    <MdDinnerDining size={30} />
+                    Dinner
+                  </h1>
+                </Link>
+                <Link to="/home/smallchop">
+                  <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+                    <GiRoastChicken size={30} />
+                    SmallChop
+                  </h1>
+                </Link>
+                <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+                  <IoIosAdd size={30} />
+                  Add Your MEAL
+                </h1>
+                <Link to="/auth/login">
+                  <button className="lg:text-2xl lg:hidden  font-bold hover:underline flex lg:flex-col flex-row items-center text-[20px] ">
+                    <IoPerson />
+                    Logout
+                  </button>
+                </Link>
+              </motion.div>
+            )}
+          </div>
+        </div>
+        <Link to="/home">
+          <h1 className="font-extrabold lg:hidden block text-3xl ">SPICE_UP</h1>
+        </Link>
+      </div>
+      <div className=" items-center  flex lg:hidden bg-white w-fit lg:px-7 px-2 py-2 lg:font-extrabold rounded-lg shadow-xl">
+        <input
+          type="search"
+          placeholder="Search...."
+          className="bg-transparent outline-none rounded-none"
+        />
+        <FaSearch size={30} className="" />
+      </div>
+    </div>
+  );
+};
+
+export default Navsmall;
+
+// <button className=" w-8 h-8" onClick={() => setOpen(!open)}>
+// <FaBarsStaggered size={30} />
+// </button>
+
+{
+  /* <div
+className=" flex lg:items-center lg:hidden  items-start flex-col justify-between lg:flex-row "
+onClick={toggleMenu}
+>
+<button className=" w-8 h-8" onClick={() => setOpen(!open)}>
+  <FaBarsStaggered size={30} />
+</button>
+<div>
+  {open && (
+    <motion.div
+      className="lg:flex-row flex-col gap-x-36 mt-8 font-extrabold   lg:flex cursor-pointer"
+      initial={{ x: -1000 }}
+      animate={{ x: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <Link to="/home/breakfast">
+        <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+          <MdFreeBreakfast size={30} />
+          Breakfast
+        </h1>
+      </Link>
+      <Link to="/home/lunch">
+        <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+          <MdLunchDining size={30} />
+          Lunch
+        </h1>
+      </Link>
+      <Link to="/home/dinner">
+        <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+          <MdDinnerDining size={30} />
+          Dinner
+        </h1>
+      </Link>
+      <Link to="/home/smallchop">
+        <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+          <GiRoastChicken size={30} />
+          SmallChop
+        </h1>
+      </Link>
+      <h1 className="hover:underline focus:underline flex lg:flex-col items-center flex-row">
+        <IoIosAdd size={30} />
+        Add Your MEAL
+      </h1>
+      <Link to="/auth/login">
+        <button className="lg:text-2xl lg:hidden  font-bold hover:underline flex lg:flex-col flex-row items-center text-[20px] ">
+          <IoPerson />
+          Logout
+        </button>
+      </Link>
+    </motion.div>
+  )}
+</div>
+</div>
+<Link to="/home">
+<h1 className="font-extrabold lg:hidden block text-3xl ">SPICE_UP</h1>
+</Link>
+</div> */
+}
